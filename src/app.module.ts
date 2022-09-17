a3c69e7407129f9cfae159f06931c36d0f1378f8
n-takeshita <naokisyuu@gmail.com>
2022-09-17 21:15:49 +0900

    add: env for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,10 +30,12 @@ function verifyAuthorizationHeader(authorization: any) {
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
           .valid('development', 'production', 'test')
           .required(),
+        DATABASE_URL: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRES_IN: Joi.string().required(),
       }),
